f042c31d9836f9bc14325c4fbaaa8946fb83d162
jirafey <you@example.com>
2024-01-07 17:29:54 +0200

    Update the script

---
+++ main.js
@@ -0,0 +1,18 @@
+function getBrowser() {
+  if( navigator.userAgent.indexOf("Chrome") != -1 ) {
+    return "Chrome";
+  } else if( navigator.userAgent.indexOf("Opera") != -1 ) {
+    return "Opera";
+  } else if( navigator.userAgent.indexOf("MSIE") != -1 ) {
+    return "IE";
+  } else if( navigator.userAgent.indexOf("Firefox") != -1 ) {
+    return "Firefox";
+  } else {
+    return "unknown";
+  }
+}
+
+    if (getBrowser() !== "Firefox") {
+                document.getElementById("browserInfo").innerHTML = "=( . w . )= _" } 
+    else {
+                document.getElementById("browserInfo").innerHTML = "=( . w . )=_"; }
