889d0ae1b27f899dcd8766e7d0dd0e4a358e14fc
jirafey <97115044+jirafey@users.noreply.github.com>
2024-01-25 12:40:47 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,19 +1,1 @@
-function getBrowser() {
-    if( navigator.userAgent.indexOf("Chrome") != -1 ) {
-        return "Chrome";
-  } else if( navigator.userAgent.indexOf("Opera") != -1 ) {
-        return "Opera";
-  } else if( navigator.userAgent.indexOf("MSIE") != -1 ) {
-        return "IE";
-  } else if( navigator.userAgent.indexOf("Firefox") != -1 ) {
-        return "Firefox";
-  } else {
-        return "unknown";
-  }
-}
-
-    if (getBrowser() !== "Firefox") {
-        document.getElementById("browserInfo").innerHTML = "=( . w . )= &#160_" } 
-    else if (getBrowser() == "Firefox") {
-        document.getElementById("browserInfo").innerHTML = "=( . w . )=_"; }
 
